5a14955af01b224a18e815cdd227c150b9761295
Arthur <arthurmueller31@gmail.com>
2025-10-06 21:52:14 -0300

    Add register license form, option to add more than one and save button

--- src/app/cadastrar-licenca/page.tsx
+++ src/app/cadastrar-licenca/page.tsx
@@ -1,13 +1,31 @@
-import Fields from "./RegisterLicense/Fields";
-import Header from "./RegisterLicense/Header";
-import SaveAndCancelButtons from "./RegisterLicense/SaveAndCancelButtons";
+// app/cadastrar-licenca/page.tsx
+import prisma from "../../../lib/prisma";
+import Header from "./_components/Header";
+import RegisterLicense from "./_components/RegisterLicense";
 
-export default function page() {
+type SearchParams = { empresaId: string } | Promise<{ empresaId: string }>;
+type Props = {
+  searchParams: SearchParams;
+};
+
+export default async function Page({ searchParams }: Props) {
+
+  // first await params, avoiding unexpected errors
+  const awaitSearchParams = await searchParams;
+
+  // convert/define searched param to number
+  const empresaIDNumber = Number(awaitSearchParams!.empresaId);
+  // const empresaId = empresaIdStr ? parseInt(empresaIdStr, 10) : null;
+
+  const empresa = await prisma.empresa.findUnique({
+    where: { id: empresaIDNumber },
+    select: { id: true, razaoSocial: true }
+  });
+
   return (
     <>
       <Header />
-      <Fields />
-      <SaveAndCancelButtons />
+      <RegisterLicense empresa={empresa} empresaId={empresa!.id} />
     </>
   );
 }
